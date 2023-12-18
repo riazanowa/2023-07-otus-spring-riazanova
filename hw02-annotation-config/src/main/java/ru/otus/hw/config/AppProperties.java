25a41f2fa933a75263008e4be94b521f0f1bb0d7
Ekaterina Riazanova <kat.riazanowa@yandex.ru>
2023-12-18 19:58:12 +0300

    hw2

---
+++ hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -0,0 +1,28 @@
+package ru.otus.hw.config;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Setter
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppProperties implements TestConfig, TestFileNameProvider {
+
+    @Value("${test.rightAnswersCountToPass}")
+    private int rightAnswersCountToPass;
+
+    @Value("${test.fileName}")
+    private String testFileName;
+
+    @Override
+    public int getRightAnswersCountToPass() {
+        return rightAnswersCountToPass;
+    }
+
+    @Override
+    public String getTestFileName() {
+        return testFileName;
+    }
+}

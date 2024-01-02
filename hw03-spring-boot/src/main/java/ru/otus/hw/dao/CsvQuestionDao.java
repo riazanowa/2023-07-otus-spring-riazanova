e5cde748e45ea881249efa8de4f584979aac1d30
Ekaterina Riazanova <kat.riazanowa@yandex.ru>
2024-01-02 20:49:49 +0300

    Add new files HW3

---
+++ hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,49 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        try (BufferedReader reader = fetchFileFromResourceAsStream(fileNameProvider.getTestFileName())) {
+            final List<QuestionDto> questionsDto = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSkipLines(1)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .build()
+                    .parse();
+
+            return questionsDto.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (IOException e) {
+            throw new QuestionReadException("Error while reading csv file: ", e);
+        }
+    }
+
+    private BufferedReader fetchFileFromResourceAsStream(String fileName) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        final String testFileName = fileNameProvider.getTestFileName();
+        InputStream inputStream = classLoader.getResourceAsStream(testFileName);
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        }
+        return new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+    }
+}
